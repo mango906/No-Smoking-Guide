0dad07356cc6a564510c8536f1b6f8b0baea7bf5
mango906 <minkb01@dgsw.hs.kr>
2019-04-19 23:06:01 +0900

    [ADD] LandingActivity

---
+++ NoSmokeGuide/app/src/main/java/dgsw/hs/kr/no_smoke_guide/Activity/LandingActivity.java
@@ -0,0 +1,35 @@
+package dgsw.hs.kr.no_smoke_guide.Activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dgsw.hs.kr.no_smoke_guide.MainActivity;
+import dgsw.hs.kr.no_smoke_guide.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    private static int TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide(); // Top Bar hide
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_landing);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LandingActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME);
+    }
+}
